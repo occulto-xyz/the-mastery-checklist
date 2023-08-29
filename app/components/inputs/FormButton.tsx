dda28a985be0759a90c0c6b027947b829bc8928b
jcbraz <josecbraz@outlook.com>
2023-08-29 15:14:08 +0100

    Add analytics

--- app/components/inputs/FormButton.tsx
+++ app/components/inputs/FormButton.tsx
@@ -8,24 +8,26 @@ export default function FormButton() {
   const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
 
   return (
-    <Button
-      className="bg-stone-50 text-stone-900 border-2 border-stone-50 hover:bg-stone-900 hover:text-stone-50 lg:w-72 w-44"
-      variant="outline"
-      type="submit"
-      size="sm"
-      onClick={() => {
-        setIsSubmitted(true);
-        setTimeout(() => {
-          setIsSubmitted(false);
-        }, 3000);
-      }}
-    >
-      {pending && <Icons.spinner className="animate-spin w-6 h-6" />}
-      {!pending && isSubmitted ? (
-        <Icons.check className="w-6 h-6" />
-      ) : (
-        "Add Task"
-      )}
-    </Button>
+    <div className="lg:pt-8 pt-0">
+      <Button
+        className="bg-stone-50 text-stone-900 border-2 border-stone-50 hover:bg-stone-900 hover:text-stone-50 lg:w-72 w-44"
+        variant="outline"
+        type="submit"
+        size="sm"
+        onClick={() => {
+          setIsSubmitted(true);
+          setTimeout(() => {
+            setIsSubmitted(false);
+          }, 3000);
+        }}
+      >
+        {pending && <Icons.spinner className="animate-spin w-6 h-6" />}
+        {!pending && isSubmitted ? (
+          <Icons.check className="w-6 h-6" />
+        ) : (
+          "Add Task"
+        )}
+      </Button>
+    </div>
   );
 }
